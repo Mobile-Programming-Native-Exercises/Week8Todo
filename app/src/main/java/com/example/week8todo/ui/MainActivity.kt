35de4d62a65953c317fb07ebef099aeb4e5f1920
PortalFan202 <anibaldonoso1@gmail.com>
2023-03-03 11:14:31 +0200

    Finished part 1

--- app/src/main/java/com/example/week8todo/ui/MainActivity.kt
+++ app/src/main/java/com/example/week8todo/ui/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.example.week8todo
+package com.example.week8todo.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.week8todo.ui.theme.Week8TodoTheme
+import com.example.week8todo.viewmodel.TodoViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TodoApp()
                 }
             }
         }
@@ -30,14 +38,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TodoApp(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<String>) {
+    LazyColumn(modifier = Modifier.padding(8.dp)) {
+        items(todos) { todo ->
+            Text(
+                text = todo,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week8TodoTheme {
-        Greeting("Android")
+        TodoApp()
     }
 }
